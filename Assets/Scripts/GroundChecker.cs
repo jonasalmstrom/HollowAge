6e90bd64e780b3452aee6a1dba6df22cc7ba2464
sgt0scarlet123 <rikard.hjulstrom@elev.ga.lbs.se>
2019-01-28 09:48:19 +0100

    Movements'N'Dash

---
+++ Assets/Scripts/GroundChecker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundChecker : MonoBehaviour
+{
+    public bool isGrounded;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Ground")
+        {
+            isGrounded = false;
+        }
+    }
+}
