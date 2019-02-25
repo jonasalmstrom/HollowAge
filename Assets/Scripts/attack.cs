39abe57dac5ff0c1d62301f9e02671764f7fc3a5
sgt0scarlet123 <rikard.hjulstrom@elev.ga.lbs.se>
2019-02-25 10:23:53 +0100

    w

---
+++ Assets/Scripts/attack.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class attack : MonoBehaviour
+{
+    public Animator attack1;
+    public SpriteRenderer this1;
+    private void Start()
+    {
+        this1 = GetComponent<SpriteRenderer>();
+        attack1 = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            this1 = true;
+            attack1.SetTrigger("Attack");
+        }
+        else
+        {
+            attack1.ResetTrigger("Attack");
+
+        }
+
+    }
+}
