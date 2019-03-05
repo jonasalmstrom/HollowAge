96e49ba291a5c176d328e5cdd4c57e4324552c60
Sebastian Eking <sebastian.eking@elev.ga.lbs.se>
2019-03-05 08:33:01 +0100

    Player Attack 2

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -1,51 +1,49 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class PlayerAttack : MonoBehaviour
-{
+//public class PlayerAttack : MonoBehaviour
+//{
 
-    GameObject[] playerAttackObject;
+//    GameObject[] playerAttackObject;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        playerAttackObject = GameObject.FindGameObjectsWithTag("Weapon");
-        HideSwordOnAttack();
-    }
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        playerAttackObject = GameObject.FindGameObjectsWithTag("Weapon");
+//        HideSwordOnAttack();
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        print("1");
 
-    }
+//        if (Input.GetKey(KeyCode.C))
+//        {
+//            print("Hit!");
+//            ShowSwordOnAttack();
+//        }
 
-    public void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (Input.GetKey(KeyCode.L))
-        {
-            print("Hit!");
-            ShowSwordOnAttack();
-        }
-        else
-        {
-            HideSwordOnAttack();
-        }
-    }
+//    }
 
-    public void ShowSwordOnAttack()
-    {
-        foreach (GameObject g in playerAttackObject)
-        {
-            g.SetActive(true);
-        }
-    }
+//    public void OnTriggerEnter2D(Collider2D collision)
+//    {
+//    }
 
-    public void HideSwordOnAttack()
-    {
-        foreach (GameObject g in playerAttackObject)
-        {
-            g.SetActive(false);
-        }
-    }
-}
+//    public void ShowSwordOnAttack()
+//    {
+//        foreach (GameObject g in playerAttackObject)
+//        {
+//            g.SetActive(true);
+//        }
+//    }
+
+//    public void HideSwordOnAttack()
+//    {
+//        foreach (GameObject g in playerAttackObject)
+//        {
+//            g.SetActive(false);
+//        }
+//    }
+//}
