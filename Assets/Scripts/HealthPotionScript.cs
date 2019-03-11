4a54803abbbd7a626cc4338874cdc1ad1a0df754
Sebastian Eking <sebastian.eking@elev.ga.lbs.se>
2019-03-11 08:35:03 +0100

    PlayerHealthPotion och PlayerPrefab

--- Assets/Scripts/HealthPotionScript.cs
+++ Assets/Scripts/HealthPotionScript.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class HealthPotionScript : MonoBehaviour
 {
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
 
         if (collision.gameObject.tag == "Player")
         {
+            GameObject Player = GameObject.Find("Player");
+            PlayerHealth playerScript = Player.GetComponent<PlayerHealth>();
+            playerScript.playerHealth += 1;
+
             Destroy(gameObject);
         }
     }
