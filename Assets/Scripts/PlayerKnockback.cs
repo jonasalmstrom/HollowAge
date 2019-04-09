93bb17e3260b5e4757801d0381718ab7bf87e488
Jonas Almström <jonas.almstrom@elev.ga.lbs.se>
2019-04-09 09:11:51 +0200

    goblin anim, run anim, knockback, fixat move script, nya sprites

--- Assets/Scripts/PlayerKnockback.cs
+++ Assets/Scripts/PlayerKnockback.cs
@@ -5,43 +5,15 @@ using UnityEngine;
 public class PlayerKnockback : MonoBehaviour
 {
 
-    public float maxX;
-    public float minX;
-    public float maxY;
-    public float minY;
+    public Rigidbody2D rb;
+    public float knockbackX;
+    public float knockbackY;
 
-    private GameObject enemy;
-    private Transform enemyTransform;
-    private Rigidbody2D rbody;
-
-    // Start is called before the first frame update
-
-    void Start()
-    {
-        rbody = GetComponent<Rigidbody2D>();
-        enemy = GameObject.FindWithTag("Damage");
-        enemyTransform = enemy.GetComponent<Transform>();
-    }
-
-    public void OnCollisionExit2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Damage")
         {
-
-            if (transform.position.x < enemyTransform.position.x)
-            {
-                print("Right");
-                rbody.velocity = new Vector2(Random.Range(-maxX, -minX), Random.Range(maxY, minY));
-            }
-
-            if (transform.position.x > enemyTransform.position.x)
-            {
-                print("Left");
-                rbody.velocity = new Vector2(Random.Range(maxX, minX), Random.Range(maxY, minY));
-            }
+            rb.GetComponent<Rigidbody2D>().velocity = new Vector2(knockbackX, knockbackY);
         }
-
     }
-
-
 }
