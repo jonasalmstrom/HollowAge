b2d597c6926643a2de55ad48c75214d1dc2f7588
Jonas Almström <jonas.almstrom@elev.ga.lbs.se>
2019-04-21 18:25:56 +0200

    bg music små fixes

--- Assets/Scripts/MoveNew.cs
+++ Assets/Scripts/MoveNew.cs
@@ -1,34 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+
 
 public class MoveNew : MonoBehaviour
 {
-    private Rigidbody2D rb;
     public float moveSpeed;
-    private float moveInput;
+    public float maxSpeed;
     public float highJumpForce;
     public float lowJumpForce;
-    
-
-    private bool isGrounded;
+    public float dashCooldown;
+    public float nextDash;
+    public float dashForce;
     public GroundChecker groundChecker;
-    private float jumpTimeCounter;
     public float jumpTime;
+    public bool lookright = false;
+    public Animator anim;
+    public float fallMultiplier;
+
+    [Header("Events")]
+    [Space]
+
+    public UnityEvent OnLandEvent;
+
+
+    private float moveInput;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    private float jumpTimeCounter;
     private bool isJumping;
     private bool allowJump;
     private bool allowHighJump;
     private bool allowDash;
+    private bool jum;
 
-    public float dashCooldown;
-    public float nextDash;
-
-    public float dashForce;
-
-
-    public bool lookright = false;
-    public Animator anim;
-    public float fallMultiplier;
+    private void Awake()
+    {
+        if (OnLandEvent == null)
+            OnLandEvent = new UnityEvent();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -41,8 +52,15 @@ public class MoveNew : MonoBehaviour
     private void FixedUpdate()
     {
         moveInput = Input.GetAxisRaw("Horizontal");
-        
-        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+
+        if (moveInput != 0)
+        {
+            rb.AddForce(new Vector2(moveInput * moveSpeed, 0), ForceMode2D.Impulse);
+        }
+        else
+        {
+            rb.velocity -= new Vector2(rb.velocity.x, 0);
+        }
 
         if (rb.velocity.y < 0)
         {
@@ -51,16 +69,33 @@ public class MoveNew : MonoBehaviour
 
         }
 
+        if (rb.velocity.x > maxSpeed)
+        {
+            rb.velocity = new Vector2(maxSpeed, rb.velocity.y);
+        }
+
+        if (rb.velocity.x < -maxSpeed)
+        {
+            rb.velocity = new Vector2(-maxSpeed, rb.velocity.y);
+        }
+
+        if (groundChecker.isGrounded && jum)
+        {
+            allowJump = true;
+            anim.SetBool("IsJumping", true);
+        }
+
         Jumps();
         DashMove();
     }
+
 
     private void Update()
     {
         nextDash += Time.deltaTime;
 
         anim.SetFloat("Speed", Mathf.Abs(moveInput));
-        
+
 
         if (Input.GetKeyDown(KeyCode.D) && lookright == false)
         {
@@ -75,19 +110,22 @@ public class MoveNew : MonoBehaviour
 
         }
 
-        if (groundChecker.isGrounded && Input.GetKeyDown(KeyCode.Space))
-        {
-            allowJump = true;
-        }
+        jum = Input.GetKeyDown(KeyCode.Space);
+
+
 
         if (Input.GetKey(KeyCode.Space) && isJumping == true)
         {
             allowHighJump = true;
+            
+
+
         }
 
         if (Input.GetKeyUp(KeyCode.Space))
         {
             isJumping = false;
+            anim.SetBool("IsJumping", false);
         }
 
         if (Input.GetKeyDown(KeyCode.LeftShift) && nextDash >= dashCooldown)
@@ -104,16 +142,22 @@ public class MoveNew : MonoBehaviour
         {
             if (lookright)
             {
-                rb.velocity += Vector2.right * dashForce * Time.deltaTime;
+                rb.AddForce(Vector2.right * dashForce, ForceMode2D.Impulse);
                 allowDash = false;
             }
             else
             {
-                rb.velocity += -Vector2.right * dashForce * Time.deltaTime;
+                rb.AddForce(-Vector2.right * dashForce, ForceMode2D.Impulse);
                 allowDash = false;
             }
 
         }
+    }
+
+    public void OnLanding()
+    {
+        anim.SetBool("IsJumping", false);
+
     }
 
     private void Flip()
@@ -140,6 +184,7 @@ public class MoveNew : MonoBehaviour
             else
             {
                 isJumping = false;
+                anim.SetBool("IsJumping", false);
             }
             if (jumpTimeCounter <= 0)
             {
