0ef16c63b157d311cbeba3f585741cb513fd4ec0
Johannestt <johannes.tornemothoren@elev.ga.lbs.se>
2019-04-30 10:08:34 +0200

    fall trapx)

---
+++ Assets/Scripts/KnockBack.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnockBack : MonoBehaviour
+{
+
+    public Rigidbody2D rb1;
+    public float knockbackX1;
+    public float knockbackY2;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            rb1.GetComponent<Rigidbody2D>().velocity = new Vector2(knockbackX1, knockbackY2);
+        }
+    }
+}
---
+++ Assets/Scripts/RotatingTrap.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingTrap : MonoBehaviour
+    
+{
+    
+    private PlayerHealth pH;
+    
+   
+    void Start()
+    {
+        pH = gameObject.GetComponent<PlayerHealth>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("aj");
+            pH.playerHealth-= 3;
+            
+        }
+    }
+}
+
