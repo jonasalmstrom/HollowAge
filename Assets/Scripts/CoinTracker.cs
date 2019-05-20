0fb1b821fbee5e16fbfe8adbc1f5e23c54a50b9e
Jonas Almström <jonas.almstrom@elev.ga.lbs.se>
2019-05-20 09:16:22 +0200

    a

--- Assets/Scripts/CoinTracker.cs
+++ Assets/Scripts/CoinTracker.cs
@@ -1,10 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CoinTracker : MonoBehaviour
 {
     public float totalCoins;
+    public Slider coinSlider;
 
-  
+
+    public void Update()
+    {
+        coinSlider.value = totalCoins;
+        print(totalCoins);
+    }
+
 }
